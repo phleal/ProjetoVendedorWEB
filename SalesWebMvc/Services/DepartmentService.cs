4f1cb69de74c5881dda6b967965353851d429638
Phillip Leal <philliplealgs@gmail.com>
2022-07-24 17:13:34 -0300

    Implementing simple search

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -9,8 +9,6 @@ namespace SalesWebMvc.Services
     public class DepartmentService
     {
         private readonly SalesWebMvcContext _context;
-
-
         public DepartmentService(SalesWebMvcContext context)
         {
             _context = context;
